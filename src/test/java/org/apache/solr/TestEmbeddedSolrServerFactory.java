33973499ee1cf66f0417595f43178d477286e501
Bryan Bende <bbende@gmail.com>
2016-10-05 21:06:41 -0400

    Fixing target location

--- src/test/java/org/apache/solr/TestEmbeddedSolrServerFactory.java
+++ src/test/java/org/apache/solr/TestEmbeddedSolrServerFactory.java
@@ -43,7 +43,7 @@ public class TestEmbeddedSolrServerFactory {
     public static void setupClass() {
         try {
             String targetLocation = EmbeddedSolrServerFactory.class
-                    .getProtectionDomain().getCodeSource().getLocation().getFile();
+                    .getProtectionDomain().getCodeSource().getLocation().getFile() + "/..";
 
             String solrHome = targetLocation + "/solr";
 
